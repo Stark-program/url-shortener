342827a557455811eced2c7c0442c85168f1b89e
Stark-program <chad.d.stark@hotmail.com>
2023-04-03 16:06:24 -0600

    Make isolated modules happy

--- src/pages/api/getUrl.ts
+++ src/pages/api/getUrl.ts
@@ -1,26 +1,27 @@
-// import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 // import { PrismaClient } from "@prisma/client";
 
 // const prisma = new PrismaClient();
 
-// const getUrl = async (req: NextApiRequest, res: NextApiResponse) => {
-//   const { urlId } = req.query;
+const getUrl = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req, res);
+  // const { urlId } = req.query;
 
-//   try {
-//     const link = await prisma.link.findUnique({
-//       where: {
-//         linkId: urlId,
-//       },
-//     });
-//     if (link) {
-//       res.status(200).json({ url: link.url });
-//     } else {
-//       res.status(404).send({ error: "No link found" });
-//     }
-//   } catch (error) {
-//     res.status(500).send({ error: "Failed to fetch data" });
-//     console.log(error);
-//   }
-// };
+  // try {
+  //   const link = await prisma.link.findUnique({
+  //     where: {
+  //       linkId: urlId,
+  //     },
+  //   });
+  //   if (link) {
+  //     res.status(200).json({ url: link.url });
+  //   } else {
+  //     res.status(404).send({ error: "No link found" });
+  //   }
+  // } catch (error) {
+  //   res.status(500).send({ error: "Failed to fetch data" });
+  //   console.log(error);
+  // }
+};
 
-// export default getUrl;
+export default getUrl;
