2bdbb32fa6aa0198441fcc6fa82bd98ed60a9a47
Stark-program <chad.d.stark@hotmail.com>
2023-04-04 18:49:40 -0600

    Add meta tags for mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 import type { AppProps } from "next/app";
 import type { Session } from "next-auth";
-
+import Head from "next/head";
 import "../styles/globals.css";
 
 const MyApp = ({
@@ -13,6 +13,26 @@ const MyApp = ({
 }>) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <meta
+          name="theme-color"
+          content="#2e026d"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#2e026d"
+          media="(prefers-color-scheme: dark)"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="apple-mobile-web-app-title" content="Snippy" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
